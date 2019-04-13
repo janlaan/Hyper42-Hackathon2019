9534e9a9091420b73db52fceb41e0ec503b0b367
Micha Wensveen <micha.wensveen@ebpi.nl>
2019-04-13 16:51:09 +0200

    call backend from phone app

--- Phone/app/src/main/java/nl/hyper42/kim/android/travel/TravelService.java
+++ Phone/app/src/main/java/nl/hyper42/kim/android/travel/TravelService.java
@@ -1,10 +1,14 @@
 package nl.hyper42.kim.android.travel;
 
 import nl.hyper42.kim.android.generated.info.FlightInfo;
+import nl.hyper42.kim.android.generated.travel.TravelDataRequest;
+import nl.hyper42.kim.android.generated.travel.TravelDataResponse;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface TravelService {
-    @GET("travelinfo")
-    Call<FlightInfo> getTravelInfo();
+    @POST("register")
+    Call<TravelDataResponse> registerFlight(@Body TravelDataRequest request);
 }
